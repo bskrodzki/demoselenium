4d57b234476a5d5a46abef75036eb268b05269da
bskrodzki <bskrodzki90@gmail.com>
2025-06-14 14:39:52 +0200

    Reapply gitignore rules

---
+++ test_register.py
@@ -0,0 +1,18 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from faker import Faker
+
+class RegistrationTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("http://seleniumdemo.com/")
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10.0)
+
+
+    def testRegistration(self):
+        self.driver.find_element(By.XPATH, "//span[@class='nav__title' and contains(text(), 'My account')]").click()
+        self.driver.find_element(By.ID,"reg_email").send_keys(Faker().email())
+        self.driver.find_element(By.ID, "reg_password").send_keys(Faker().password())
+        self.driver.find_element(By.XPATH, "//button[@name='register']").click()
