e9eee8a991ba6219f273e3ae982ef594a1e57b5b
bskrodzki <bskrodzki90@gmail.com>
2025-06-15 09:14:24 +0200

    new test

--- test_register.py
+++ test_register.py
@@ -2,6 +2,7 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from faker import Faker
+import os
 
 class RegistrationTest(unittest.TestCase):
     def setUp(self):
@@ -13,6 +14,8 @@ class RegistrationTest(unittest.TestCase):
 
     def testRegistration(self):
         self.driver.find_element(By.XPATH, "//span[@class='nav__title' and contains(text(), 'My account')]").click()
-        self.driver.find_element(By.ID,"reg_email").send_keys(Faker().email())
+        email = os.environ.get("EMAIL")
+        self.driver.find_element(By.ID, "reg_email").send_keys(email)
+        # self.driver.find_element(By.ID,"reg_email").send_keys(Faker().email())
         self.driver.find_element(By.ID, "reg_password").send_keys(Faker().password())
         self.driver.find_element(By.XPATH, "//button[@name='register']").click()
